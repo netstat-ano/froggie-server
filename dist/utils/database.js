a64ce83eacc4eafe9776f684e055dafba650628b
netstat-ano <bartosz147258@gmail.com>
2022-12-19 16:26:28 +0100

    build script added

--- dist/utils/database.js
+++ dist/utils/database.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const sequelize = new sequelize_1.Sequelize("froggie", "root", "root", {
+const sequelize = new sequelize_1.Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     dialect: "mysql",
-    host: "localhost",
+    host: process.env.DB_HOST,
 });
 exports.default = sequelize;
